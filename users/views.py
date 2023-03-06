93063c2ef21dc7a725ac1b07e1663b446ec98240
Marvin <taipe.marvin17@gmail.com>
2023-03-06 17:44:10 -0500

    modi

--- users/views.py
+++ users/views.py
@@ -2,12 +2,10 @@ from django.contrib.auth.models import User
 from .serializer import UserSerializer
 from rest_framework import viewsets
 from django.contrib.auth.hashers import make_password
-from django.views.decorators.csrf import requires_csrf_token
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @requires_csrf_token
     def perform_create(self, serializer):
         serializer.save(password=make_password(serializer.validated_data['password']))
