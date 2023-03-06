97231b04eb6a8fee5e2dc5a74b1a55a4c917b0c9
Marvin <taipe.marvin17@gmail.com>
2023-03-06 16:43:54 -0500

    csrf_exempt en views.py users

--- users/views.py
+++ users/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from .serializer import UserSerializer
 from rest_framework import viewsets
 from django.contrib.auth.hashers import make_password
+from django.views.decorators.csrf import csrf_exempt
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @csrf_exempt
     def perform_create(self, serializer):
         serializer.save(password=make_password(serializer.validated_data['password']))
