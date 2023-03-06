b497bfa8ee109e7b6e810fdc79563a76c134ed81
Marvin <taipe.marvin17@gmail.com>
2023-03-06 13:10:20 -0500

    modificando archivos estaticos

--- movies/settings.py
+++ movies/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-^r2oor132r(v!xxe4#p13yk+^dngg7)v8_wv=g5%bh1dnd%#uh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "movieapidjango-production.up.railway.app"]
+CSRF_TRUSTED_ORIGIN = ["https://movieapidjango-production.up.railway.app"]
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,7 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
